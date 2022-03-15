c54f9c57b038656d8065eaecc7703c849856c50a
WaDadidou <loic_thomas@outlook.com>
2022-03-15 14:13:41 +0100

    - Linted.

--- src/components/_layouts/Main/Main.jsx
+++ src/components/_layouts/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Provider} from 'react-redux'
 import store from '/src/redux/store'
-import Hello from "../../Hello/Hello";
+import Hello from '../../Hello/Hello'
 
 const Main = () => {
 
